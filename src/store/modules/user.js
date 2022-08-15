34ef0b2b784f1329b210ebe9e82291e1a837aaba
吴作栋 <10342860+goh-chok-tong@user.noreply.gitee.com>
2022-08-15 23:12:07 +0800

    init

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,39 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user' // 引入登录接口
+// 状态
+const state = {
+  // 设置token为共享状态
+  token: getToken() //设置token为共享状态  初始化的时候，先从缓存里面读取
+}
+// 修改状态
+const mutations = {
+  setToken(state, token) {
+    // 将数据设置给Vuex
+    state.token = token
+    // 同步给缓存
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null //将vuex的数据置空
+    removeToken()
+  }
+}
+// 执行异步
+const actions = {
+  async login(content, data) {
+    // 调用api接口
+    const result = await login(data)
+    if (result.data.success) {
+      // 表示登录接口调用成功 也就是意味着你的用户名和密码是正确的
+      // 现在有用户token
+      // actions 修改state 必须通过mutations
+      content.commit('setToken', result.data.data)
+    }
+  }
+}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 const service = axios.create()
+// 请求拦截器
 service.interceptors.request.use()
+// 响应拦截器
 service.interceptors.response.use()
 export default service
